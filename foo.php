13cabfeb26327c232bf96373c2c827db0cb66b73
Jonathan Buch <jbuch@synyx.de>
2019-05-31 09:25:48 +0200

    Add dark theme

--- foo.php
+++ foo.php
@@ -7,14 +7,18 @@
 <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.1/css/bootstrap-combined.min.css" rel="stylesheet">
 <link rel="stylesheet" href="css/blinkftw.css">
 <link rel="stylesheet" href="css/main.css">
-
+<?php
+if (isset($extra_css)) {
+    echo "<link rel=\"stylesheet\" href=\"{$extra_css}\">";
+}
+?>
+</head>
+<body>
 <?php
 date_default_timezone_set('Europe/Berlin');
 if (date("G") >= 20){
 	echo "<marquee><center><blink><h1>Alles wird gut! &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; echt!</h1></blink></center></marquee>";
 }
-?>
 
-<?php 
 flush();
 include("nagdash.php");
