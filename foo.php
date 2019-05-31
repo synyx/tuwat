d765e772f2e1e0155aa84983edd8c787a1f17283
Jonathan Buch <jbuch@synyx.de>
2019-05-31 07:54:49 +0200

    Add changes for foo/index

--- foo.php
+++ foo.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 <title>Nagios Dashboard</title>
-<meta http-equiv="refresh" content="90; URL=http://contargo-nagdash.synyx.coffee/">
+<meta http-equiv="refresh" content="90; URL=https://contargo-nagdash.synyx.coffee/">
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.1/js/bootstrap.min.js"></script>
 <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.1/css/bootstrap-combined.min.css" rel="stylesheet">
--- index.php
+++ index.php
@@ -1,56 +1,43 @@
-<?php
-@session_start();
-error_reporting(E_ALL ^ E_NOTICE);
-require_once('config.php');
-require_once('utils.php');
-if (isset($_REQUEST['known'])){
-	$_SESSION['known'] = $_REQUEST['known'];
-} else {
-	$_SESSION['known'] = "autooff";
-}
-
-$unwanted_hosts = unserialize($_COOKIE['nagdash_unwanted_hosts']);
-if (!is_array($unwanted_hosts)) $unwanted_hosts = array();
-?>
 <html>
 <head>
 <title>Nagios Dashboard</title>
-<meta http-equiv="refresh" content="10; URL=http://contargo-nagdash.synyx.coffee/">
+<meta http-equiv="refresh" content="90; URL=https://contargo-nagdash.synyx.coffee/">
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.1/js/bootstrap.min.js"></script>
 <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.1/css/bootstrap-combined.min.css" rel="stylesheet">
 <link rel="stylesheet" href="css/blinkftw.css">
 <link rel="stylesheet" href="css/main.css">
-<script>
-  function showInfo(show_data) {
-      $("#info-window").fadeIn("fast");
-      $("#info-window-text").empty().append(show_data);
-  }
-  $(document).ready(function() {
-      $("#nagioscontainer").load("nagdash.php", function() { $("#spinner").fadeOut("fast"); });
-      var refreshId = setInterval(function() {
-          <?if ($show_refresh_spinner) {?>
-          $("#spinner").fadeIn("fast");
-          <? }?>
-          $("#nagioscontainer").load("nagdash.php", function() { $("#spinner").fadeOut("fast"); });
-      }, 20000);
-      $.ajaxSetup({ cache: false });
-  });
-  $(document).keypress("s", function(e) {
-      $("#settings_modal").modal();
-  });
-</script>
-<style type="text/css">
-  <?php foreach ($nagios_hosts as $host) { 
-        echo ".tag_{$host['tag']}   { background-color: {$host['tagcolour']} }\n"; 
-  } ?>
-</style>
-</head>
-<body>
-  <div id="spinner"><h3><img src="images/ajax-loader.gif" align="absmiddle"> Refreshing...</h3></div>
-  <div id="nagioscontainer"></div>
-	<?=build_settings_dialog($nagios_hosts, $unwanted_hosts) ?>
-<a href="?known=on">known on</a> - 
-<a href="?known=off">known off</a>
-</body>
-</html>
+
+<?php
+date_default_timezone_set('Europe/Berlin');
+if (date("G") >= 20){
+	echo "<marquee><center><blink><h1>Alles wird gut! &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; echt!</h1></blink></center></marquee>";
+}
+?>
+
+<?php 
+flush();
+include("nagdash.php")
+
+##$ddate = file_get_contents('http://api.ddate.cc/v1/today.txt');
+#$ddate = file_get_contents('/var/www/dash/Nagdash/ddate.txt');
+#echo "<center><h3>$ddate</h3><br>";
+#echo "<h3>";
+#include("temp.txt");
+#echo "°C</h3></center>";
+# $biertime = "not sure if its biertime…";
+#if (preg_match('/<title>(.+)<\/title>/',file_get_contents('http://bier.synyx.de'),$matches) && isset($matches[1])){
+#  $biertime = $matches[1];
+#  }
+#else{
+#     $biertime = "not sure if its biertime…";
+#}
+##echo "<center><h3>$biertime</h3></center><br>".date("R");
+#echo "<center><h3>$biertime</h3></center><br>";
+
+#include("synyx.html");
+
+
+#flush();
+#include("foo2.php");
+?>
