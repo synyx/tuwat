9d9ab180e9c5c459f7df65bacfd6dd4ba21bbee4
Jonathan Buch <jbuch@synyx.de>
2021-02-19 15:07:33 +0100

    patchman, use new sec/bugfix totals

--- connectors/patchman.php
+++ connectors/patchman.php
@@ -5,10 +5,14 @@
  * @return string | array Error as string
  */
 function connectPatchman($url) {
-  #return apcu_entry("patchman-{$url}", 'patchmanHosts', 3600);
-  return patchmanLogin($url);
+  $hosts = patchmanGet($url . "/api/hosts/");
+  return patchmanHosts2Nagios($hosts);
 }
 
+/**
+ * @param $hosts array
+ * @return array|string Error as string
+ */
 function patchmanHosts2Nagios($hosts) {
 
   if (count($hosts) == 0) {
@@ -24,9 +28,9 @@ function patchmanHosts2Nagios($hosts) {
 
     $startsAt = DateTime::createFromFormat('Y-m-d\TH:i:s+', $host['lastreport'],  new DateTimeZone('Etc/Zulu'));
 
-    if ($host['security'] > 0) {
+    if ($host['security_update_count'] > 0) {
       $sn                          = 'Patch level insufficient';
-      $description = "Security updates: {$host['security']}, Updates: {$host['updates']}";
+      $description = "Security updates: {$host['security_update_count']}, Updates: {$host['bugfix_update_count']}";
       $hosts[$hn]['services'][$sn] = [
         'current_state'                 => 1,
         'problem_has_been_acknowledged' => 0,
@@ -47,73 +51,6 @@ function patchmanHosts2Nagios($hosts) {
 
   #print_r($host_state);
   return array(array_key_first($host_state) => $host_state[array_key_first($host_state)]);
-}
-
-function patchmanLogin(string $url) {
-  $html = patchmanGet($url . '/login/');
-  $doc = new DOMDocument();
-  $doc->loadHTML($html);
-  $inputs = $doc->getElementsByTagName('input');
-
-  $csrftoken = null;
-  foreach ($inputs as $input) {
-    foreach ($input->attributes as $attr) {
-      if ($attr->nodeValue == 'csrfmiddlewaretoken') {
-        foreach ($input->attributes as $vattr) {
-          if ($vattr->nodeName == 'value') {
-            $csrftoken = $vattr->nodeValue;
-          }
-        }
-      }
-    }
-  }
-
-  if ($csrftoken) {
-    $username    = parse_url($url, PHP_URL_USER);
-    $password    = parse_url($url, PHP_URL_PASS);
-
-    $data = [
-      'csrfmiddlewaretoken' => $csrftoken,
-      'username' => $username,
-      'password' => $password,
-      'next' => '/dashboard/',
-    ];
-    patchmanPost($url . '/login/', $data);
-
-    $hosts = array();
-
-    $html = patchmanGet($url . '/dashboard/');
-    $doc = new DOMDocument();
-    $doc->loadHTML($html);
-
-    $updates_node = $doc->getElementById('bugupdate_hosts');
-    $updates = $updates_node ? $updates_node->getElementsByTagName('tr') : [];
-    foreach ($updates as $update) {
-      [$hostname, $nr] = patchmanExtractUpdatesFromNode($update, 2);
-      if (!isset($hosts[$hostname])) $hosts[$hostname] = array('hostname' => $hostname);
-      $hosts[$hostname]['updates'] = intval($nr);
-    }
-
-    $secupdates_node = $doc->getElementById('secupdate_hosts');
-    $secupdates = $secupdates_node ? $secupdates_node->getElementsByTagName('tr') : [];
-    foreach ($secupdates as $update) {
-      [$hostname, $nr] = patchmanExtractUpdatesFromNode($update, 1);
-      if (!isset($hosts[$hostname])) $hosts[$hostname] = array('hostname' => $hostname);
-      $hosts[$hostname]['security'] = intval($nr);
-    }
-
-    return $hosts;
-  }
-  return [];
-}
-
-function patchmanExtractUpdatesFromNode(DOMElement $node, int $column) {
-  $tds = $node->getElementsByTagName('td');
-  $host = $tds->item(0)->textContent;
-  $nr = $tds->item($column)->textContent;
-  $lastreport = $tds->item(5)->textContent;
-
-  return [$host, $nr, $lastreport];
 }
 
 function patchmanGet(string $url, $params = array()) {
@@ -149,47 +86,14 @@ function patchmanGet(string $url, $params = array()) {
   }
   $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
   curl_close($ch);
-  $header = substr($response, 0, $header_size);
+  #$header = substr($response, 0, $header_size);
   $body = substr($response, $header_size);
-  #print_r($header);
-  return $body;
-}
 
-
-function patchmanPost(string $url, array $payload) {
-  $hostname    = parse_url($url, PHP_URL_HOST);
-  $port        = parse_url($url, PHP_URL_PORT);
-  $request_url = $url;
-  $headers     = [
-    'Accept: */*',
-  ];
-  $ch          = curl_init();
-  curl_setopt_array(
-    $ch, [
-      CURLOPT_URL            => $request_url,
-      CURLOPT_HTTPHEADER     => $headers,
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_HEADER         => true,
-      #CURLOPT_SSL_VERIFYHOST => 2,
-      #CURLOPT_SSL_VERIFYPEER => 1
-      CURLOPT_SSL_VERIFYHOST => 0,
-      CURLOPT_SSL_VERIFYPEER => 0,
-      CURLOPT_POSTFIELDS     => http_build_query($payload),
-      CURLOPT_CUSTOMREQUEST  => 'POST',
-      CURLOPT_COOKIEFILE     => '/tmp/nagdash_cookiejar.txt',
-      CURLOPT_COOKIEJAR      => '/tmp/nagdash_cookiejar.txt',
-      CURLOPT_VERBOSE        => true,
-    ]
-  );
-  if (!$response = curl_exec($ch)) {
-    return ["<pre>Attempt to post to patchman failed, sorry. Curl said: " . curl_error($ch) . "</pre>"];
-  } else {
-    $curl_stats["$hostname:$port"] = curl_getinfo($ch);
+  if (!$state = json_decode($body, true)) {
+    return "Attempt to parse gitlab json failed, sorry (JSON decode failed)";
   }
-  $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-  curl_close($ch);
-  $header = substr($response, 0, $header_size);
-  $body = substr($response, $header_size);
+  $curl_stats["$hostname:$port"]['objects'] += count($state);
+
   #print_r($header);
   return $body;
 }
