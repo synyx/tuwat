8c8bac721ab3b9779a2971c2d6be76a89842507b
Jonathan Buch <jbuch@synyx.de>
2023-07-03 08:47:11 +0200

    redmine, add connector for due dates
    * Show as critical when due date is in the past
    * Show as warning if due date is today

---
+++ pkg/connectors/redmine/connector.go
@@ -0,0 +1,106 @@
+package redmine
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	html "html/template"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/synyx/tuwat/pkg/connectors"
+	"github.com/synyx/tuwat/pkg/connectors/common"
+	"github.com/uptrace/opentelemetry-go-extra/otelzap"
+	"go.uber.org/zap"
+)
+
+type Connector struct {
+	config Config
+	client *http.Client
+}
+
+type Config struct {
+	Tag string
+	common.HTTPConfig
+}
+
+func NewConnector(cfg *Config) *Connector {
+	return &Connector{*cfg, cfg.HTTPConfig.Client()}
+}
+
+func (c *Connector) Tag() string {
+	return c.config.Tag
+}
+
+func (c *Connector) Collect(ctx context.Context) ([]connectors.Alert, error) {
+	issues, err := c.collectIssues(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var alerts []connectors.Alert
+	for _, issue := range issues {
+		due, _ := time.Parse("2006-01-02", issue.DueDate)
+		state := connectors.Critical
+		if issue.DueDate == time.Now().Format("2006-01-02") {
+			state = connectors.Warning
+		}
+
+		alert := connectors.Alert{
+			Labels: map[string]string{
+				"Ticket": fmt.Sprintf("#%d", issue.Id),
+				"Source": c.config.URL,
+				"Due":    issue.DueDate,
+				"Type":   "Ticket",
+			},
+			Start:       due,
+			State:       state,
+			Description: issue.Subject,
+			Details:     issue.Description,
+			Links: []html.HTML{
+				html.HTML("<a href=\"" + c.config.URL + "/issues/" + strconv.Itoa(issue.Id) + "\" target=\"_blank\" alt=\"Home\">🏠</a>"),
+			},
+		}
+		alerts = append(alerts, alert)
+		continue
+
+	}
+
+	return alerts, nil
+}
+
+func (c *Connector) String() string {
+	return fmt.Sprintf("Redmine (%s)", c.config.URL)
+}
+
+func (c *Connector) collectIssues(ctx context.Context) ([]issue, error) {
+	otelzap.Ctx(ctx).Debug("getting issues", zap.String("url", c.config.HTTPConfig.URL+"/issues.json"))
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, c.config.HTTPConfig.URL+"/issues.json", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("assigned_to", "me")
+	req.Header.Set("status_id", "open")
+	req.Header.Set("due_date", "<="+time.Now().Format("2006-01-02"))
+	req.Header.Set("X-Redmine-API-Key", c.config.BearerToken)
+
+	res, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	decoder := json.NewDecoder(res.Body)
+
+	var response response
+	err = decoder.Decode(&response)
+	if err != nil {
+		return nil, err
+	}
+
+	return response.Issues, nil
+}
