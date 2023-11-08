4bbe2ddb623c1b148535f1a70e64406afb897a30
Jonathan Buch <jbuch@synyx.de>
2023-11-08 11:11:25 +0100

    Enable matching on numbers

--- pkg/config/rule_matching.go
+++ pkg/config/rule_matching.go
@@ -1,19 +1,102 @@
 package config
 
-import "regexp"
+import (
+	"math"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+const (
+	gt = iota
+	eq
+	ge
+	lt
+	le
+)
 
 type RuleMatcher interface {
 	MatchString(s string) bool
+}
+
+func ParseRuleMatcher(label, s string) RuleMatcher {
+	if strings.HasPrefix(s, "~= ") {
+		return regexpMatcher{regexp.MustCompile(s)}
+	} else if strings.HasPrefix(s, "> ") {
+		return newNumberMatcher(gt, s)
+	} else if strings.HasPrefix(s, "> ") {
+		return newNumberMatcher(gt, s)
+	} else if strings.HasPrefix(s, "= ") {
+		if _, err := strconv.ParseFloat(s, 64); err == nil {
+			return newNumberMatcher(eq, s)
+		} else {
+			return equalityMatcher{s}
+		}
+	} else if strings.HasPrefix(s, ">= ") {
+		return newNumberMatcher(ge, s)
+	} else if strings.HasPrefix(s, "< ") {
+		return newNumberMatcher(lt, s)
+	} else if strings.HasPrefix(s, "<= ") {
+		return newNumberMatcher(le, s)
+	} else {
+		return regexpMatcher{regexp.MustCompile(s)}
+	}
 }
 
 type regexpMatcher struct {
 	r *regexp.Regexp
 }
 
-func (r regexpMatcher) MatchString(s string) bool {
-	return r.r.MatchString(s)
+func (m regexpMatcher) MatchString(s string) bool {
+	return m.r.MatchString(s)
 }
 
-func ParseRuleMatcher(k, v string) RuleMatcher {
-	return regexpMatcher{regexp.MustCompile(v)}
+type numberMatcher struct {
+	operation int
+	number    float64
+}
+
+func newNumberMatcher(op int, s string) numberMatcher {
+	number, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		panic("config parsing error")
+	}
+	return numberMatcher{op, number}
+}
+
+func (m numberMatcher) MatchString(s string) bool {
+	number, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		// an invalid number cannot be compared, thus returns as non-matching
+		return false
+	}
+
+	switch m.operation {
+	case gt:
+		return floatEqualEnough(m.number, number) && m.number > number
+	case eq:
+		return floatEqualEnough(m.number, number)
+	case ge:
+		return floatEqualEnough(m.number, number) || m.number > number
+	case lt:
+		return !floatEqualEnough(m.number, number) && m.number < number
+	case le:
+		return floatEqualEnough(m.number, number) || m.number > number
+	}
+
+	return false
+}
+
+const epsilon = 1e-8
+
+func floatEqualEnough(a, b float64) bool {
+	return math.Abs(a-b) <= epsilon
+}
+
+type equalityMatcher struct {
+	s string
+}
+
+func (m equalityMatcher) MatchString(s string) bool {
+	return m.s == s
 }
