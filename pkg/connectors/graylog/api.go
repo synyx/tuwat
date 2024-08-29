48d85050e6b6da5db035840b3f32e78d9e7cd1c1
Jonathan Buch <jbuch@synyx.de>
2024-08-29 13:42:37 +0200

    graylog, remove unused legacy alert structures

--- pkg/connectors/graylog/api.go
+++ pkg/connectors/graylog/api.go
@@ -1,19 +1,5 @@
 package graylog
 
-type alertResult struct {
-	Total  int     `json:"total"`
-	Alerts []alert `json:"alerts"`
-}
-
-type alert struct {
-	Id          string `json:"id"`
-	Description string `json:"description"`
-	ConditionId string `json:"condition_id"`
-	StreamId    string `json:"stream_id"`
-	TriggeredAt string `json:"triggered_at"`
-	ResolvedAt  string `json:"resolved_at"`
-}
-
 type eventsSearchParameters struct {
 	Query     string             `json:"query"`
 	Page      int                `json:"page"`
