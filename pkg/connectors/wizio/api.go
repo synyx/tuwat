7f8beba7323ba47c4c83e091fbbf09fe378a28d0
Oliver Schmitt <oschmitt@contargo.net>
2025-04-29 15:57:50 +0200

    Drafted a connector for wizio issue api

---
+++ pkg/connectors/wizio/api.go
@@ -0,0 +1,50 @@
+package wizio
+
+import "time"
+
+type issuesResponse struct {
+	Data data `json:"data"`
+}
+type data struct {
+	IssuesV2 issuesV2 `json:"issuesV2"`
+}
+
+type issuesV2 struct {
+	TotalCount int    `json:"totalCount"`
+	Nodes      []node `json:"nodes"`
+}
+
+type node struct {
+	Id             string         `json:"id"`
+	Status         string         `json:"status"`
+	Severity       string         `json:"severity"`
+	CreatedAt      time.Time      `json:"createdAt"`
+	UpdatedAt      time.Time      `json:"updatedAt"`
+	Control        control        `json:"control"`
+	EntitySnapshot entitySnapShot `json:"entitySnapshot"`
+	Project        project        `json:"project"`
+	SourceRules    []sourceRule   `json:"sourceRules"`
+}
+
+type control struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type entitySnapShot struct {
+	Id            string `json:"id"`
+	Type          string `json:"type"`
+	Name          string `json:"name"`
+	Status        string `json:"status"`
+	CloudPlatform string `json:"cloudPlatform"`
+	Region        string `json:"region"`
+}
+
+type project struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type sourceRule struct {
+	Name string `json:"name"`
+}
---
+++ pkg/connectors/wizio/connector.go
@@ -0,0 +1,193 @@
+package wizio
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/synyx/tuwat/pkg/connectors"
+	"github.com/synyx/tuwat/pkg/connectors/common"
+	"io"
+	"log/slog"
+	"net/http"
+)
+
+type Connector struct {
+	config Config
+	client *http.Client
+}
+
+type Config struct {
+	Tag string
+	common.HTTPConfig
+}
+
+func NewConnector(cfg *Config) *Connector {
+	return &Connector{*cfg, cfg.HTTPConfig.Client()}
+}
+
+func (c *Connector) Tag() string {
+	return c.config.Tag
+}
+
+func (c *Connector) Collect(ctx context.Context) ([]connectors.Alert, error) {
+	issueResponse, err := c.collectIssues(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var alerts []connectors.Alert
+
+	for _, node := range issueResponse.Data.IssuesV2.Nodes {
+		descr := "Issue: " + node.Control.Name
+		alert := connectors.Alert{
+			Labels: map[string]string{
+				"Entity":     node.EntitySnapshot.Name,
+				"EntityType": node.EntitySnapshot.Type,
+				"Status":     node.Status,
+				//"Assignee":  mr.Assignee.Name,
+				"Source": c.config.URL,
+			},
+			Start:       node.CreatedAt,
+			State:       mapState(node.Severity),
+			Description: descr,
+			// Details:     details,
+			//Links: []html.HTML{
+			//	html.HTML("<a href=\"" + mr.WebUrl + "\" target=\"_blank\" alt=\"Home\">🏠</a>"),
+			//},
+		}
+		alerts = append(alerts, alert)
+	}
+
+	return alerts, nil
+}
+
+func (c *Connector) String() string {
+	return fmt.Sprintf("Wiz.io Issues (%s)", c.config.URL)
+}
+
+func (c *Connector) collectIssues(ctx context.Context) (*issuesResponse, error) {
+	// TODO
+	graphqlQuery := `
+		query IssuesTable(
+			$filterBy: IssueFilters $first: Int $after: String $orderBy: IssueOrder
+			) {
+				issuesV2(
+					filterBy: $filterBy
+					first: $first
+					after: $after
+					orderBy: $orderBy
+				) { 
+					nodes {
+						id
+						control {
+							id 
+							name
+						}
+						createdAt 
+						updatedAt
+						dueAt
+						project {
+							id
+							name
+							slug
+							businessUnit
+							riskProfile {
+								businessImpact
+							}
+						}
+						status
+						severity
+						entitySnapshot {
+							id
+							type
+							name
+							status 
+							cloudPlatform
+							region
+						}
+						note
+						serviceTickets {
+							externalId
+							name
+							url
+						}
+					}
+					pageInfo {
+						hasNextPage
+						endCursor
+					}
+				}
+			 }
+    `
+	query := struct {
+		Query     string                 `json:"query"`
+		Variables map[string]interface{} `json:"variables"`
+	}{
+		Query: graphqlQuery,
+		Variables: map[string]interface{}{
+			"first": 10,
+		},
+	}
+
+	from := "/graphql"
+
+	marshal, err := json.Marshal(query)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := c.get(ctx, from, string(marshal))
+	if err != nil {
+		return nil, err
+	}
+
+	decoder := json.NewDecoder(body)
+
+	var issues issuesResponse
+	err = decoder.Decode(&issues)
+	slog.InfoContext(ctx, "Decoded response", issues)
+	if err != nil {
+		slog.ErrorContext(ctx, "Cannot parse",
+			slog.String("url", c.config.URL+from),
+			slog.Any("error", err),
+		)
+		return nil, err
+	}
+
+	return &issues, nil
+}
+
+func (c *Connector) get(ctx context.Context, endpoint string, query string) (io.ReadCloser, error) {
+	slog.DebugContext(ctx, "getting alerts", slog.String("url", c.config.URL+endpoint))
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, c.config.URL+endpoint, bytes.NewBuffer([]byte(query)))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Content-Type", "application/json")
+
+	res, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return res.Body, nil
+}
+
+func mapState(severity string) connectors.State {
+	switch severity {
+	case "CRITICAL":
+		return connectors.Critical
+	case "HIGH":
+		return connectors.Warning
+	case "MEDIUM":
+		return connectors.Warning
+	case "LOW":
+		return connectors.Warning // TODO Do we want to output Low Issues?
+	default:
+		return connectors.Unknown
+	}
+}
